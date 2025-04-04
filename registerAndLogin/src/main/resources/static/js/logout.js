424f583e3ffc2bff4d994ceee3d5ead01eecced8
Narendrasai1656383 <narendrasai547@gmail.com>
2025-04-04 13:45:56 +0530

    commit

---
+++ registerAndLogin/src/main/resources/static/js/logout.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const logoutForm=document.getElementById("logoutForm");
+	if (logoutForm) {
+	        logoutForm.addEventListener("submit", (e) => {
+	            e.preventDefault();
+	            fetch(`http://localhost:8080/auth/logout`, {
+	                method: "GET",
+	                credentials: "include" 
+	            })
+	            .then(response => {
+	                if (!response.ok) {
+	                    return response.text().then(text => Promise.reject(text));
+	                }
+	                return response.text();
+	            })
+	            .then(data => {
+	                alert(data); 
+	                window.location.href = "index.html";
+	            })
+	            .catch(error => {
+	                console.error("Login error:", error);
+	                alert(error); 
+					window.location.href = "index.html";
+	            });
+	        });
+	    }
+	})
