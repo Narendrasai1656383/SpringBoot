0c724e315e4b65da540b155fed20f1d51150dfe8
Narendrasai1656383 <narendrasai547@gmail.com>
2025-04-09 11:02:17 +0530

    commit

--- registerAndLogin/src/main/resources/static/js/auth.js
+++ registerAndLogin/src/main/resources/static/js/auth.js
@@ -1,8 +1,3 @@
-window.history.replaceState(null, "", window.location.href);
-window.addEventListener("popstate", function () {
-    window.history.replaceState(null, "", window.location.href);
-});
-
 const API_BASE_URL = "http://localhost:8080/auth";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -32,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => {
                 alert(data); 
 				localStorage.setItem("isActive","yes");
-                window.location.href = "products.html";
+				window.location.replace("products.html");
             })
             .catch(error => {
                 console.error("Login error:", error);
@@ -61,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             .then(result => {
                 alert(result); 
-                window.location.href = "login.html";
+				window.location.replace("login.html");
             })
             .catch(error => {
                 console.error("Registration error:", error);
